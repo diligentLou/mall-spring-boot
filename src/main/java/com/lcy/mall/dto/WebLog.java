c60a7ac334c8ee264bd65fec16ccf3c827ef9c95
lcy <328895398@qq.com>
2020-07-23 22:51:33 +0800

    feat:Aop

---
+++ src/main/java/com/lcy/mall/dto/WebLog.java
@@ -0,0 +1,69 @@
+package com.lcy.mall.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @description:Controller层的日志封装类
+ * @author:lcy
+ * @createTime:2020/7/23 22:13
+ */
+@Getter
+@Setter
+public class WebLog {
+
+    /**
+     * 操作描述
+     */
+    private String description;
+
+    /**
+     * 操作用户
+     */
+    private String username;
+
+    /**
+     * 操作时间
+     */
+    private Long startTime;
+
+    /**
+     * 消耗时间
+     */
+    private Integer spendTime;
+
+    /**
+     * 根路径
+     */
+    private String basePath;
+
+    /**
+     * URI
+     */
+    private String uri;
+
+    /**
+     * URL
+     */
+    private String url;
+
+    /**
+     * 请求类型
+     */
+    private String method;
+
+    /**
+     * IP地址
+     */
+    private String ip;
+
+    /**
+     * 请求参数
+     */
+    private Object parameter;
+
+    /**
+     * 请求返回的结果
+     */
+    private Object result;
+}
